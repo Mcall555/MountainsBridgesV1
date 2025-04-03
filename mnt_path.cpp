ae182dfde0d61617e010a2bcd75ece2bd0843d2f
Gurshan Singh <105119199+Mcall555@users.noreply.github.com>
2025-04-02 22:02:23 -0700

    Clean up and re-arranging
    Read notes and comments for more detail.

--- mnt_path.cpp
+++ mnt_path.cpp
@@ -1,38 +1,41 @@
+/* 
+   Hello Ridha, all I did as of 4/2/2025 @ 10:01 PM was clean up the code, rearranged some stuff, and fixed indentation.
+   I plan on doing more soon, ttyl. -Gurshan
+*/
+
+// imported from Bridges
 #include "Bridges.h"
 #include "ColorGrid.h"
+#include "DataSource.h"
+#include "data_src/ElevationData.h"
 
+// imported from C++
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
 #include <limits>
 
-#include "DataSource.h"
-#include "data_src/ElevationData.h"
-
+// namespaces for ease of access 
 using namespace std;
 using namespace bridges;
 
+/*
+  This program implements the mountain paths assignment in C++. See the README.md for a
+  detailed description
+  takes in the processed elevation data and returns a color grid for
+  visualization using BRIDGES 
+ */
 
-// This program implements the mountain paths assignment in C++. See the README.md for a
-// detailed description
-
-
-// takes in the processed elevation data and returns a color grid for
-// visualization using BRIDGES
 ColorGrid getImage(const ElevationData& elev_data) {
-  //TODO
+  // TODO
 
   /*
    return a Color Grid that represents the elevation as gray scale
-  
    access elevation with elev_data.getVal(row, col)
    compute the intensity by linear interpolation between elev_data.getMinVal(); and elev_data.getMaxVal();
-  
    create ColorGrid with ColorGrid(nbrows, nbcols)
-
    fill ColorGrid with .set(row, col, color) 
-
   */
 
   ColorGrid cg (1,1); //stub
@@ -41,16 +44,19 @@ ColorGrid getImage(const ElevationData& elev_data) {
 
 }
 
-// determines the least effort path through the mountain starting a point on
-// the left edge of the image
+ 
+// determines the least effort path through the mountain starting a point on the left edge of the image
 
 void findPath(const ElevationData&  elev_data, int startRow, ColorGrid& cg) {
-  //        Run the greedy path from (0, startRow) to the right of the image
-  //
-  // always move right, but select the right cell, the top right cell, or bottom right cell
-  // by minimizing the difference of elevation.
-  //
-  // Write path to the colorgrid
+
+  // TODO
+  
+  /*
+   Run the greedy path from (0, startRow) to the right of the image
+   always move right, but select the right cell, the top right cell, or bottom right cell
+   by minimizing the difference of elevation.
+   Write path to the colorgrid 
+   */
 }
 
 int main(int argc, char **argv) {
@@ -58,6 +64,8 @@ int main(int argc, char **argv) {
   /*
     bridges object initialization
     initialize Bridges
+
+    How to use our function:
     bridges(saveSlot,userId,API_Key);
   */
 
@@ -71,14 +79,13 @@ int main(int argc, char **argv) {
   bridges.setDescription("Your goal is to find a path that takes you through the points with the lowest elevation changes, in an effort to minimize the overall effort in walking through the path.");
 
   // get elevation data
-
   DataSource ds;
   ElevationData elev_data = ds.getElevationData(6.02, 44.10, 9.70, 47.77, 0.02);
 
   // color grid for visualization
   ColorGrid cg = getImage(elev_data);
 
-  // find path by applying a greedy algorithm
+  // find path by applying a greedy algorithm (the function we wrote above)
   startRow = elev_data.getRows()/2;
   findPath (elev_data, startRow, cg);
 
@@ -86,5 +93,5 @@ int main(int argc, char **argv) {
   bridges.setDataStructure(cg);
   bridges.visualize();
 
-  return 0;
+  return 0; // end of program. Horray! :D
 }
