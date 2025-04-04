ccdb0efc951ae5dd46e8f7fbe0b93546943fe086
Gurshan Singh <105119199+Mcall555@users.noreply.github.com>
2025-04-04 15:25:38 -0700

    Update mnt_path.cpp

--- mnt_path.cpp
+++ mnt_path.cpp
@@ -116,7 +116,7 @@ int main(int argc, char **argv) {
 	cout << endl;
 
 	if (userChoice == 1) {
-	     run(1,"Mcall555",1301845300906,"Avoid Hillbilly Hell", "Your goal is to find the path with least elevation change from one side to the other");
+	     run(1,"Mcall555","1301845300906","Avoid Hillbilly Hell", "Your goal is to find the path with least elevation change from one side to the other");
 	} else if (userChoice == 2){
              run(); //Read what I wrote in function for implamentaiton
 	}
