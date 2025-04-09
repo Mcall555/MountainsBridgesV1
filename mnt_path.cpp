9bbc132f1a3a9f75af5347ad9a5007f2dfc19098
GurshanSingh <0944145@my.scccd.edu>
2025-04-09 01:20:06 -0700

    Holy shit im a dumbass.

--- mnt_path.cpp
+++ mnt_path.cpp
@@ -1,14 +1,5 @@
- /* 
-   Hello Teamate, all I did as of 4/7/2025 was clean up the code, rearranged some stuff, and fixed indentation.
-   I plan on doing more soon, ttyl. -Gurshan
-
-    How to run the code
-    --> https://imgur.com/a/iCZkUe7 <--
-   
- */
 
 // imported from Bridges
-
 #include "Bridges.h"
 #include "ColorGrid.h"
 #include "DataSource.h"
@@ -44,28 +35,27 @@ ColorGrid getImage(const ElevationData& elevData) {
     fill ColorGrid with .set(row, col, color) 
 
     💫 return a Color Grid that represents the elevation as gray scale
-
   */
-  	
-    cout << "\tWidth: " << elevData.getCols() << endl;
-	cout << "\tHeight: " << elevData.getRows() << endl;
-	cout << "\tCell Size: " << elevData.getCellSize() << endl;
-	cout << "\tLower Left Corner: " << elevData.getxll() << ", " << elevData.getyll() << endl;
-
-	cout << "The first 10 elevation values: " << endl;
-    
-	for (int k = 0; k < 10; k++){
-       cout << "\t" << elevData.getVal(0,k) << endl;
-    }
-	
-	cout << endl;
-	
-	
-  //cout << elevData.getVal(row,col) << endl;
    	
-  ColorGrid cg (1,1); //stub
-  
-  return cg;
+	Color grey(128,128,128);
+    ColorGrid cg (elevData.getRows(),elevData.getCols(), grey);
+
+	int minVal = elevData.getMinVal();
+	int maxVal = elevData.getMaxVal();
+
+	for(int row = 0; row < elevData.getRows(); row++){
+      for(int col = 0; col < elevData.getCols(); col++){
+        int val = elevData.getVal(row,col);
+
+		int newShade = 255 * (val - minVal) / (maxVal - minVal);
+		Color newColor(newShade,newShade,newShade);
+
+		cg.set(row,col,newColor);
+
+      }
+	}
+
+    return cg;
 
 }
 
@@ -81,16 +71,16 @@ void findPath(const ElevationData&  elev_data, int startRow, ColorGrid& cg) {
     always move right, but select the right cell, the top right cell, or bottom right cell
     by minimizing the difference of elevation.
     
-    💫 Write path to the colorgrid 
-    
+    💫 Write path to the colorgrid  
   */
+
 	 int rows = elev_data.getRows();
 	 int cols = elev_data.getCols();
 	 int row = startRow;
 
 	 bridges::Color red(255, 0, 0);
 
-	 cg.set(row, 0, red);					//draw the starting point
+	 cg.set(row, 0, red);//draw the starting point
 
 	 for (int col = 0; col < cols - 1; col++) {
 		int currentVal = elev_data.getVal(row, col);
@@ -103,33 +93,35 @@ void findPath(const ElevationData&  elev_data, int startRow, ColorGrid& cg) {
 		 //Check up-right (diagonal)
 		if (row > 0) {
 			int change = abs(currentVal - elev_data.getVal(row - 1, nextCol));
-		if (change < minChange) {
-			minChange = change;
-			bestRow = row - 1;
-			 }
+		    if (change < minChange){
+			  minChange = change;
+			  bestRow = row - 1;
+			}
 		 }
 		//Check right (straight)
 		{
 		int change = abs(currentVal - elev_data.getVal(row, nextCol));
-			if (change < minChange) {
-				minChange = change;
-				bestRow = row;
-		}
-	 }
+			if (change < minChange){
+			  minChange = change;
+			  bestRow = row;
+		   }
+	     }
 		//Check down-right (diagonal)
 		 if (row < rows - 1) {
 			int change = abs(currentVal - elev_data.getVal(row + 1, nextCol));
-		if (change < minChange) {
-			minChange = change;
-			bestRow = row + 1;
-			 }
+		     if (change < minChange){
+		       minChange = change;
+		       bestRow = row + 1;
+			}
 		 }
 
 		//Moves to next position
 		row = bestRow; 
 		//Draw red at the final column
-		cg.set(row, cols - 1, red);
+		cg.set(row, col, red); 
+		
 }
+
  }
 
 int main(int argc, char **argv) {
@@ -139,38 +131,44 @@ int main(int argc, char **argv) {
 	   💫 How to use our function:
 	    bridges(saveSlot,userId,API_Key);
 	  */
-try {
-	      //Make sure all of the paramaters are strings
-	      Bridges bridges(1, "Mcall555", "1301845300906");
-		
+        try {
+	     	 //Make sure all of the paramaters are strings
+	      	 Bridges bridges(1, "Mcall555", "1301845300906");
 	
-	      // defaults for row number and data file
-	      int startRow = 50;
-	  
-	      // set title
-	      bridges.setTitle("Avoid Hillbilly Hell");
-	
-	      bridges.setDescription("Your goal is to find the path with least elevation change from one side to the other");
-	
-	      // get elevation data
-	      DataSource ds;
-	      ElevationData elev_data = ds.getElevationData(6.02, 44.10, 9.70, 47.77, 0.02);
-	
-	      // color grid for visualization
-	      ColorGrid cg = getImage(elev_data);
-	
-	      // find path by applying a greedy algorithm (the function we wrote above)
-	      startRow = elev_data.getRows()/2; //The middle. (middle of the left side I think.)
-	      findPath (elev_data, startRow, cg);
-	
-	      // visualize
-	      bridges.setDataStructure(cg);
-	      bridges.visualize();
+	     	 // defaults for row number and data file
+	     	 //int startRow = 50;
 
-  return 0; // end of program. Yippeeee!
-	 } catch (const char* msg) {
-			cerr << "Exception caught: " << msg << endl;
-	 }
+	  		 int startRow = 50;
+			 
+	     	 // set title
+	     	 bridges.setTitle("Least Altitude Change Path.");
+	
+	     	 bridges.setDescription("Your goal is to find the path with least elevation change from one side to the other");
+	
+	    	 // get elevation data
+	     	 DataSource ds;
+		
+	     	 ElevationData elev_data = ds.getElevationData(40.03133177632377, -98.02593749997456, 42.508577297430456, -96.94531249997696, 0.0166);
+
+	    	  // color grid for visualization
+
+	         ColorGrid cg = getImage(elev_data);
+	
+	    	 // find path by applying a greedy algorithm (the function we wrote above)
+
+	         startRow = elev_data.getRows() / 2; //The middle. (middle of the left side I think.)
+
+	    	 findPath(elev_data, startRow, cg);
+	
+	    	 // visualize
+	    	 bridges.setDataStructure(cg);
+	    	 bridges.visualize();
+
+        	 return 0;
+
+	        } catch (const char* msg) {
+			  cerr << "Exception caught: " << msg << endl;
+	 		}
 }	
 
 
